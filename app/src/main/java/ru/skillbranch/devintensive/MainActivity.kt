1f7f8a8917a2caa575a198b3f1b5f22e35b91d50
zdt1985 <zdt1985@yandex.ru>
2019-07-21 11:04:54 +0700

    hometask_3 последнее задание

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.InputType
 import android.util.Log
 import android.view.View
--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -3,38 +3,24 @@ package ru.skillbranch.devintensive.extensions
 import android.app.Activity
 import android.content.Context
 import android.graphics.Rect
-import android.view.View
 import android.view.inputmethod.InputMethodManager
-import kotlin.math.roundToLong
-import android.util.TypedValue
 
-fun Activity.hideKeyboard() {
+    fun Activity.hideKeyboard() {
     val focus = this.currentFocus
     focus?.let {
         (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.let {
             it.hideSoftInputFromWindow(focus.windowToken, 0)
+           }
         }
-    }
-
-    fun Activity.convertDpToPx(dp: Float): Long {
-        val r = this.resources
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP, dp, r.displayMetrics).roundToLong()
     }
 
     fun Activity.isKeyboardOpen(): Boolean {
         val r = Rect()
-        val rootView = findViewById<View>(android.R.id.content)
-        rootView.getWindowVisibleDisplayFrame(r)
-        val heightDiff = rootView.height - r.height()
-        val marginOfError = this.convertDpToPx(50F)
-
-        return heightDiff > marginOfError
+        window.decorView.getWindowVisibleDisplayFrame(r)
+        return window.decorView.height - (r.bottom - r.top) > window.decorView.height / 4
     }
 
     fun Activity.isKeyboardClosed(): Boolean {
-        return this.isKeyboardOpen().not()
+        return !isKeyboardOpen()
     }
-}
-
 
