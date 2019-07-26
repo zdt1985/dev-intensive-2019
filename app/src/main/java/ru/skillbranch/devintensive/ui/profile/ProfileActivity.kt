9c01c5baa9171d8f7374d3ee7bd59324c52b39e5
zdt1985 <zdt1985@yandex.ru>
2019-07-26 23:28:38 +0700

    lesson_4 2

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,49 +1,90 @@
 package ru.skillbranch.devintensive.ui.profile
 
-import android.graphics.Color
+import android.graphics.ColorFilter
 import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
-import android.util.Log
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
-import ru.skillbranch.devintensive.extensions.hideKeyboard
-import ru.skillbranch.devintensive.models.Bender
 
 class ProfileActivity : AppCompatActivity() {
+    companion object {
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
 
-    /**
-     * Вызывается при первом создании или перезапуске Activity.
-     *
-     * здесь задаётся внешний вил активности (UI) через метод setContentView().
-     * инициализируются с необходимыми данными и ресурсами
-     * связываются данные со списками
-     *
-     * Этот метод также представляет Bundle, содержащий ранее сохраненное
-     * состояние Activity, если оно было.
-     *
-     * Всегда сопровождается вызовом onStart().
-     */
+    var isEditMode = false
+    lateinit var viewFields : Map<String, TextView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        initViews(savedInstanceState)
+
     }
 
-      /**
-     * Этот метод сохраняет  представления в Bundle
-     * Для API Level < 28 (Android P) этот метод будет выполняться до onStop(), и нет никаких гарантий относительно того,
-     * произойдет ли это до или после onPause().
-     * Для API Level >= 28 будет вызван после onStop()
-     * Не будет вызван если Activity будет явно закрыто пользвователем при нажатии на системную клавишу back
-     */
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields = mapOf(
+            "nickname" to tv_nick_name,
+            "rank" to tv_rank,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository,
+            "rating" to tv_rating,
+            "respect" to tv_respect
+        )
+
+        isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+        showCurrentMode(isEditMode)
+
+        btn_edit.setOnClickListener{
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        }
+    }
+
+    private fun showCurrentMode(isEdit: Boolean) {
+        val info = viewFields.filter { setOf("firstName","lastName","about","repository").contains(it.key) }
+        for ((_,v) in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if (isEdit) 255 else 0
+        }
+
+        ic_eye.visibility = if (isEdit) View.GONE else View.VISIBLE
+        wr_about.isCounterEnabled = isEdit
+
+        with(btn_edit){
+            val filter: ColorFilter? = if(isEdit){
+                PorterDuffColorFilter(
+                    resources.getColor(R.color.color_accent, theme),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }else{
+                null
+            }
+
+            val icon = if (isEdit){
+                resources.getDrawable(R.drawable.ic_save_black_24dp)
+            }else{
+                resources.getDrawable(R.drawable.ic_edit_black_24dp)
+            }
+
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
+    }
+
+
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
+        outState?.putBoolean(IS_EDIT_MODE, isEditMode)
     }
 
 }
