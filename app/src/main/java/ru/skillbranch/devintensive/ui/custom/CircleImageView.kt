50cac9c1d898bfdbc137e74ca3bf4571bdf95033
zdt1985 <zdt1985@yandex.ru>
2019-07-28 13:17:03 +0700

    hometask_4 4

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -0,0 +1,52 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.ImageView
+import androidx.annotation.ColorRes
+import androidx.annotation.Dimension
+import androidx.core.content.ContextCompat
+import ru.skillbranch.devintensive.App
+import ru.skillbranch.devintensive.R
+
+class CircleImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): ImageView(context, attrs, defStyleAttr) {
+    companion object {
+        private const val DEFAULT_BORDER_COLOR: Int = Color.WHITE
+    }
+
+    private var borderColor = DEFAULT_BORDER_COLOR
+    private var borderWidth = 2
+
+    init {
+        if (attrs != null) {
+            val attrVal = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
+            borderColor = attrVal.getColor(R.styleable.CircleImageView_cv_borderColor, DEFAULT_BORDER_COLOR)
+            borderWidth = attrVal.getDimensionPixelSize(R.styleable.CircleImageView_cv_borderWidth, borderWidth)
+            attrVal.recycle()
+        }
+    }
+
+    @Dimension
+    private fun getBorderWidth():Int = borderWidth
+
+    private fun setBorderWidth(@Dimension dp:Int) {
+        borderWidth = dp
+        this.invalidate()
+    }
+
+    private fun getBorderColor():Int = borderColor
+
+    private fun setBorderColor(hex:String) {
+        borderColor = Color.parseColor(hex)
+        this.invalidate()
+    }
+    private fun setBorderColor(@ColorRes colorId: Int) {
+        borderColor = ContextCompat.getColor(App.applicationContext(), colorId)
+        this.invalidate()
+    }
+}
